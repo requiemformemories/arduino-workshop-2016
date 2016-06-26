2044d44ecb7cee6195496f28f8b944661e2e0afc
HuaiShaoChang <lfhfhvu@gmail.com>
2016-06-26 11:08:31 +0800

    Update TestCode004.ino

--- LEDMatrixForN/TestCode004.ino
+++ LEDMatrixForN/TestCode004.ino
@@ -21,6 +21,22 @@ void setup() {
     ledController.clear();
     Serial.begin(9600);
     Serial.println("Setting Complete!");
+    ledController.setColumn(0, 1);
+    ledController.setColumn(1, 2);
+    ledController.setColumn(2, 3);
+    ledController.setColumn(3, 4);
+    ledController.setColumn(4, 16);
+    ledController.setColumn(5, 32);
+    ledController.setColumn(6, 64);
+    ledController.setColumn(7, 255);
+    ledController.setColumn(8, 127);
+    ledController.setColumn(9, 63);
+    ledController.setColumn(10, 31);
+    ledController.setColumn(11, 15);
+    ledController.setColumn(12, B11100000);
+    ledController.setColumn(13, B10100000);
+    ledController.setColumn(14, B00111100);
+    ledController.setColumn(15, B11000011);
 }
 
 void loop() {
